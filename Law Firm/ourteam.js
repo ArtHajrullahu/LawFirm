0ef5a43821fb2eaa7ce4f274535601ab17acde8c
ArtHajrulahu <114111460+ArtHajrullahu@users.noreply.github.com>
2023-05-30 23:36:43 +0200

    Add files via upload

---
+++ Law Firm/ourteam.js
@@ -0,0 +1,40 @@
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var member1 = document.getElementById('member1');
+    var member2 = document.getElementById('member2');
+    var member3 = document.getElementById('member3');
+    var member4 = document.getElementById('member4');
+
+    member1.innerHTML = `
+    <img src="Foto1.jpg" alt="Team Member 1">
+    <h2>Adnan Krasniqi</h2>
+    <p>Avokati i cili formoi kete kompani te avokateve</p>
+    `;
+    member2.innerHTML = `
+    <img src="foto2.jpg" alt="Team Member 2">
+    <h2>Rexhep Rexhepi</h2>
+    <p>Avokati i cili merret me dokumentet e rastit.</p>
+    `;
+    member3.innerHTML = `
+    <img src="foto3.jpg" alt="Team Member 3">
+    <h2>Luan Gashi</h2>
+    <p>Avokati i cili merret me raste penale.</p>
+    `;
+    member4.innerHTML = `
+    <img src="Foto4.jpg" alt="Team Member 4">
+    <h2>Diellza Berisha</h2>
+    <p>Asistentja e cila merret me sherbimet e avokateve.</p>
+    `;
+    member5.innerHTML = `
+    <img src="foto5.jpg" alt="Team Member 5">
+    <h2>Leonora Halili</h2>
+    <p>Avokatja e cila merret me seancat e e te gjithve.</p>
+    `;
+    member6.innerHTML = `
+    <img src="Foto6.jpg" alt="Team Member 6">
+    <h2>Leotrim Spahiu</h2>
+    <p>Asistenti i cili merret me sherbimet e njerzve.</p>
+    `;
+  });
+  
